df1c25be583c91b12eaed7c82dc39c4ec9746291
daniele.falchetti <daniele.falchetti@ibm.com>
2022-02-28 18:22:22 +0100

    jk

--- src/components/listIndex.vue
+++ src/components/listIndex.vue
@@ -5,16 +5,25 @@
   >
     <span v-if="blok.title" class="block text-xl p-5" v-text="blok.title" />
     <ul
-      :class="`grid gap-y-5 ${
+      :class="`${
         blok.body.some(
-          (item) =>
-            item.drop_text && !item.drop_text.replace(/\s/g, '') === '``'
+          (item) => item.drop_text && removeBackticks(item.drop_text)
         )
           ? 'px-3 py-5 md:px-5'
           : 'p-5'
       }`"
     >
-      <item v-for="(item, index) in blok.body" :blok="item" :key="index" />
+      <item
+        v-for="(item, index) in blok.body"
+        :blok="item"
+        :key="index"
+        :class="[
+          { 'pb-5': index === 0 },
+          { 'pt-5': index === blok.body.length - 1 },
+          { 'border-b': index !== blok.body.length - 1 },
+          { 'py-5': index !== blok.body.length - 1 && index !== 0 },
+        ]"
+      />
     </ul>
   </div>
 </template>
@@ -22,6 +31,7 @@
 import item from "./itemIndex.vue";
 export default {
   components: { item },
+  inject: ["removeBackticks"],
   props: {
     blok: {
       type: Object,
